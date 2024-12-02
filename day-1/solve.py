8e108e8004f961ef433614820e44b61edfbcca8c
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-12-01 22:59:07 -0500

    Adds day one part two solution

--- day-1/solve.py
+++ day-1/solve.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from pathlib import Path
 
 
@@ -8,8 +9,8 @@ class Solver:
     - Within each pair, figure out how far apart the two numbers are; you'll need to add up all of those distances.
     """
 
-    def read_input(self) -> list[list[int]]:
-        lines = Path("input.txt").read_text().splitlines()
+    def read_input(self, filename: str = "input.txt") -> list[list[int]]:
+        lines = Path(filename).read_text().splitlines()
         rows: list[list[int]] = []
         for line in lines:
             line_parts: list[str] = line.split()
@@ -38,3 +39,27 @@ class Solver:
         answer = sum(distances)
 
         return answer
+
+    def get_similarity_score(self, rows: list[list[int]]) -> int:
+        """
+        Get a tally of distances that appear in each set
+
+        :param rows:
+        :return:
+        """
+        # c_1 = Counter()
+        c_2 = Counter()
+        col_1 = self.get_cols_sorted(rows, 0)
+        col_2 = self.get_cols_sorted(rows, 1)
+        score = 0
+
+        # for left in col_1:
+        #    c_1[left] += 1
+
+        for right in col_2:
+            c_2[right] += 1
+
+        for num in col_1:
+            score += num * c_2[num]
+
+        return score
--- day-1/test.py
+++ day-1/test.py
@@ -1,10 +1,25 @@
+import pytest
+
 from solve import Solver
+
+solver = Solver()
+
+
+@pytest.fixture(autouse=True, scope="session")
+def input_fixture() -> list[list[int]]:
+    rows = solver.read_input()
+    assert len(rows) == 1000
+    return rows
 
 
 def test_solve():
-    solver = Solver()
-    rows = solver.read_input()
-    assert len(rows) == 1000
-
     answer = solver.solve()
     print(f"\nanswer is {answer}")
+
+
+def test_get_similarity_score(input_fixture: list[list[int]]):
+    # rows = solver.read_input("example.txt")
+    rows = solver.read_input()
+    similarity_score = solver.get_similarity_score(rows)
+    # assert similarity_score == 31
+    print(f"similarity_score is {similarity_score}")
