a9de780ba7a539543f48bfc77472251e77fe8d4b
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-12-02 20:55:01 -0500

    Adds new test

--- day-2/solver.py
+++ day-2/solver.py
@@ -1,21 +1,58 @@
+from pathlib import Path
+
+
 def is_safe(levels: list[int]) -> bool:
-    c = 0
     num_levels = len(levels)
     max_diff = 3
     min_diff = 1
-    is_increasing = False
-    is_decreasing = False
-    for level in levels:
+    num_increases = 0
+    num_decreases = 0
+    for c, level in enumerate(levels):
         if c + 1 < num_levels:
             next_level = levels[c + 1]
             diff = abs(level - next_level)
             min_diff_failed = diff < min_diff
             max_diff_failed = diff > max_diff
             if min_diff_failed or max_diff_failed:
+                """
                 print(
                     f"diff is {diff}, min_diff_failed {min_diff_failed}, max_diff_failed {max_diff_failed}"
                 )
+                """
                 return False
-        c += 1
 
-    return True
+            if c > 0:
+                if level > levels[c - 1]:
+                    num_increases += 1
+                else:
+                    num_decreases += 1
+
+                is_decreasing = level < levels[c - 1]
+                is_increasing = level > levels[c - 1]
+                if is_increasing and num_decreases > 0:
+                    return False
+                if is_decreasing and num_increases > 0:
+                    return False
+
+                # No change
+                is_last_level = c == num_levels - 1
+                if is_last_level and num_decreases == 0 and num_increases == 0:
+                    return False
+
+    all_increasing = num_increases == num_levels
+    all_decreasing = num_decreases == num_levels
+
+    if all_increasing or all_decreasing:
+        return True
+
+
+def get_safe_reports_count() -> int:
+    reports = Path("./input.txt").read_text().splitlines()
+    num_safe_reports = 0
+
+    for report in reports:
+        levels = [int(l) for l in report.split()]
+        if is_safe(levels):
+            num_safe_reports += 1
+
+    return num_safe_reports
--- day-2/test.py
+++ day-2/test.py
@@ -1,4 +1,4 @@
-from solver import is_safe
+from solver import get_safe_reports_count, is_safe
 
 
 def test_is_safe():
@@ -9,8 +9,15 @@ def test_is_safe():
     # Unsafe because 2 7 is an increase of 5.
     assert not is_safe([9, 7, 6, 2, 1])
     # Unsafe because 1 3 is increasing but 3 2 is decreasing.
-    assert not is_safe([1, 3, 2, 4, 5])
+    assert not is_safe([1, 3, 2, 4, 5]), "Unsafe because decreasing"
     # Unsafe because 4 4 is neither an increase or a decrease.
-    assert not is_safe([8, 6, 4, 4, 1])
+    assert not is_safe([8, 6, 4, 4, 1]), "Unsafe because not increasing or decreasing"
     # Safe because the levels are all increasing by 1, 2, or 3.
-    assert is_safe([1, 3, 6, 7, 9])
+    assert is_safe([1, 3, 6, 7, 9]), "Safe because all levels increasing"
+
+
+def test_get_safe_reports_count():
+    safe_reports_count = get_safe_reports_count()
+    assert safe_reports_count > 0
+    assert safe_reports_count < 563
+    print(f"\n{safe_reports_count}")
