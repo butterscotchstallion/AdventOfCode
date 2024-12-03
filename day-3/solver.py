ea14a2543d8fa2c4f4fced1884be370087b507ce
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-12-03 00:27:53 -0500

    Adds day three solution

---
+++ day-3/solver.py
@@ -0,0 +1,18 @@
+import logging
+import re
+
+logger = logging.getLogger(__name__)
+
+
+def get_total_from_pairs(instructions: str) -> int:
+    pattern = re.compile(r"mul\((\d+,\d+)\)")
+    matches = pattern.findall(instructions)
+    total = 0
+
+    logger.debug(f"{len(matches)} matches: {matches}")
+
+    for operands in matches:
+        digits = operands.split(",")
+        total += int(digits[0]) * int(digits[1])
+
+    return total
--- day-3/test.py
+++ day-3/test.py
@@ -1,2 +1,24 @@
+import logging
+from pathlib import Path
+
+from solver import get_total_from_pairs
+
+logger = logging.getLogger(__name__)
+
+
+def test_solver_example():
+    example_input = (
+        "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    )
+    actual = get_total_from_pairs(example_input)
+
+    assert actual == 161
+
+
 def test_solver():
-    pass
+    instructions = Path("input.txt").read_text().splitlines()
+    total = 0
+    for instruction in instructions:
+        total += get_total_from_pairs(instruction)
+
+    logger.info(f"Total: {total}")
